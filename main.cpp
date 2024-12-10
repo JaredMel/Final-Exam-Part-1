1071e44a809f3745607772749b96fb82e36de525
JaredMel <jareddmelendez1764@gmail.com>
2024-12-09 18:43:20 -0800

    WIP

--- main.cpp
+++ main.cpp
@@ -6,7 +6,26 @@
 #include <stack>
 using namespace std;
 
+const int SIZE = 10;
+
+struct Node
+{
+    string name;
+    string order;
+    Node *next;
+
+    Node(string new_name, string new_order)
+    {
+        name = new_name;
+        order = new_order;
+        next = nullptr;
+    }
+};
+
+
 int main()
 {
+    string names[SIZE] = {"James", "Jared", "Dominc", "Bob", "Jeffery", "Jessica", "Lisa", "Eric", "Jess", "Willy"};
+    string orders[SIZE] = {"Coffee", "Pumpkin Spice Latte", "Latte", "Hot Coco", "Expresso", "Water", "Cotton Candy Latte", "Peppermint Coco", "Mocha", "Strawberry Mocha"};
     
 }
