eceb3bc09203fe30ea2cb43720ff2e76c8b6a1e9
JaredMel <jareddmelendez1764@gmail.com>
2024-12-09 19:57:16 -0800

    WIP

--- main.cpp
+++ main.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <list>
-#include <set>
-#include <map>
 #include <deque>
-#include <stack>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
+//Initilize and Declare Variables
 const int SIZE = 10;
 const int CYCLES = 10;
-
+//Node struct
 struct Node
 {
+    //Declare attributes
     string name;
     string order;
     Node *next;
-
+    //Node Constructor
     Node(string new_name, string new_order)
     {
         name = new_name;
@@ -24,12 +23,13 @@ struct Node
         next = nullptr;
     }
 };
-
+//Muffin struct
 struct Muffin
 {
+    //Declare attributes
     string name;
     string order;
-
+    //Muffin Constructor
     Muffin()
     {
         string names[SIZE] = {"James", "Jared", "Dominc", "Bob", "Jeffery", "Jessica", "Lisa", "Eric", "Jess", "Willy"};
@@ -67,7 +67,7 @@ struct Games
     Games()
     {
         string names[SIZE] = {"James", "Jared", "Dominc", "Bob", "Jeffery", "Jessica", "Lisa", "Eric", "Jess", "Willy"};
-        string orders[SIZE] = {"Mario", "Sonic", "GTA", "Elden Ring", "Mario Kart", "Orange bracelet", "Green bracelet", "Purple bracelet", "Black bracelet", "White bracelet"};
+        string orders[SIZE] = {"Mario", "Sonic", "GTA", "Elden Ring", "Mario Kart", "Mario Party", "Fortnight", "COD", "Doom", "Bloodborne"};
         int ran1 = rand() % 10;
         int ran2 = rand() % 10;
         name = names[ran1];
@@ -85,6 +85,7 @@ int main()
     Node *head = nullptr;
     deque<Muffin> muffinBooth;
     vector<Bracelets> braceletBooth;
+    list<Games> gameBooth;
     
     int prob;
     int count = 1;
@@ -107,6 +108,12 @@ int main()
     for (size_t i = 0; i < 3; i++)
     {
         braceletBooth.push_back(Bracelets());
+    }
+
+    cout << "   Game Booth:" << endl;
+    for (size_t i = 0; i < 3; i++)
+    {
+        gameBooth.push_back(Games());
     }
     
 
@@ -158,29 +165,43 @@ int main()
         {
             cout << "       Line is empty" << endl;
         }
+
+        cout << "   Game Booth:" << endl;
+        prob = rand() % 100 + 1;
+
+        if (prob <= 50)
+        {
+            gameBooth.push_back(Games());
+        }
+        if (!gameBooth.empty())
+        {
+            cout <<  "       " << gameBooth.front().name << " was served" << endl;
+            gameBooth.pop_front();
+        }
+        else
+        {
+            cout << "       Line is empty" << endl;
+        }
     }
 }
 
-//the addNodeToTail function
 Node* addCustomer(Node * &hd)
 {
-    //Declares and initilizes variables
     string names[SIZE] = {"James", "Jared", "Dominc", "Bob", "Jeffery", "Jessica", "Lisa", "Eric", "Jess", "Willy"};
     string orders[SIZE] = {"Coffee", "Pumpkin Spice Latte", "Latte", "Hot Coco", "Expresso", "Water", "Cotton Candy Latte", "Peppermint Coco", "Mocha", "Strawberry Mocha"};
     int ran1 = rand() % 10;
     int ran2 = rand() % 10;
 
-    //creates new_node
     Node* new_node = new Node(names[ran1], orders[ran2]);
     cout << "       " << names[ran1] << " has ordered a " << orders[ran2] << endl;
-    //checks if list is empty
+
     if (hd == nullptr)
     {
         return new_node;
     }
-    //creates last
+    
     Node * last = hd;
-    //iterates through the list in till it reachs the end
+    
     while (last->next != nullptr)
     {
         last = last->next;
